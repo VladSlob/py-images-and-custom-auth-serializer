1ada9dec4a4a245638530bb004a97ab68316eb9c
VladSlob <141657806+VladSlob@users.noreply.github.com>
2025-04-23 12:10:19 +0300

    serializers.py

--- user/serializers.py
+++ user/serializers.py
@@ -1,7 +1,5 @@
-from django.contrib.auth import get_user_model
-from django.contrib.auth import authenticate
-from django.utils.translation import gettext as _
-
+from django.contrib.auth import get_user_model, authenticate
+from django.utils.translation import gettext_lazy as _
 from rest_framework import serializers
 
 
@@ -10,10 +8,13 @@ class UserSerializer(serializers.ModelSerializer):
         model = get_user_model()
         fields = ("id", "email", "password", "is_staff")
         read_only_fields = ("is_staff",)
-        extra_kwargs = {"password": {"write_only": True,
-                                     "min_length": 5,
-                                     "style": "password"}
-                        }
+        extra_kwargs = {
+            "password": {
+                "write_only": True,
+                "min_length": 5,
+                "style": {"input_type": "password"},
+            }
+        }
 
     def create(self, validated_data):
         """Create a new user with encrypted password and return it"""
@@ -32,12 +33,12 @@ class UserSerializer(serializers.ModelSerializer):
 
 class AuthTokenSerializer(serializers.Serializer):
     email = serializers.CharField(
-        label=_("Email address"),
+        label=_("Email"),
         write_only=True
     )
     password = serializers.CharField(
         label=_("Password"),
-        style={"style": "password"},
+        style={"input_type": "password"},
         trim_whitespace=False,
         write_only=True
     )
@@ -63,6 +64,5 @@ class AuthTokenSerializer(serializers.Serializer):
         else:
             msg = _('Must include "email" and "password".')
             raise serializers.ValidationError(msg, code="authorization")
-
         attrs["user"] = user
         return attrs
